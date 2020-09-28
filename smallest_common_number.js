28c67b5242387f264ea7d4620fae88e0b9055050
Nick <knackepizza@gmail.com>
2020-09-28 14:59:31 -0400

    Error handling

--- smallest_common_number.js
+++ smallest_common_number.js
@@ -1,23 +1,26 @@
 function smallest_common_number(arr1, arr2, arr3) {
-    // Declare the lowest number with NO value so if it never gets set, it will be undefined 
-    let lowest;
+    // If sufficient arrays are passed
+    if ((arr1 != undefined) && (arr2 != undefined) && (arr3 != undefined)) {
+        // Declare the lowest number with NO value so if it never gets set, it will be undefined 
+        let lowest;
 
-    // Sort arr1 in ascending order
-    arr1 = arr1.sort();
+        // Sort arr1 in ascending order
+        arr1 = arr1.sort();
 
-    // Go backwards in the array to get lower and lower each loop iteration
-    for (let i = arr1.length; i >= 0; i--) {
-        // If the current value is spotted in both arrays, it becomes the new lowest value
-        if ((arr2.indexOf(arr1[i]) != -1) && (arr3.indexOf(arr1[i])) != -1) {
-            lowest = arr1[i];
+        // Go backwards in the array to get lower and lower each loop iteration
+        for (let i = arr1.length; i >= 0; i--) {
+            // If the current value is spotted in both arrays, it becomes the new lowest value
+            if ((arr2.indexOf(arr1[i]) != -1) && (arr3.indexOf(arr1[i])) != -1) {
+                lowest = arr1[i];
+            }
         }
-    }
 
-    // If there is no number found (lowest is undefined), return false
-    if (lowest != undefined) {
-        return lowest;
-    } else {
-        return false;
+        // If there is no number found (lowest is undefined), return false
+        if (lowest != undefined) {
+            return lowest;
+        } else {
+            return false;
+        }
     }
 }
 
